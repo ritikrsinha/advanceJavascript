055b02b74640a5380f37710c1ae7a8de77f9c2ab
ritik sinha <110479671+ritikrsinha@users.noreply.github.com>
2022-09-07 17:07:03 +0530

    button

---
+++ index.js
@@ -0,0 +1,10 @@
+function attachEventListeners(){
+let count = 0;
+document.getElementById("clickMe").addEventListener("click", function xyz(){
+    console.log('Button Clicked', ++count);
+ });
+}
+attachEventListeners()
+
+
+
